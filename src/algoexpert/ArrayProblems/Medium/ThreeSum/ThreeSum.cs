26ab1f8fb58d1ba87b94fcff6ad6e460ae716e88
phyothiha-rezerv <phyothihacodes@gmail.com>
2024-05-18 17:22:51 +0700

    feat : three sum

---
+++ src/algoexpert/ArrayProblems/Medium/ThreeSum/ThreeSum.cs
@@ -0,0 +1,41 @@
+﻿namespace algoexpert;
+
+public class ThreeSum
+{
+    public static List<int[]> Run(int[] array, int targetSum)
+    {
+        List<int[]> result = [];    // space n
+        Array.Sort(array);
+        for (int i = 0; i < array.Length - 2; i++)  // time n
+        {
+            // get remaining sum
+            int remainingNumber = targetSum - array[i];
+
+            // find if there's any number can add up to remaining sum
+            // need to skip the index of current number
+            int left = i + 1;
+            int right = array.Length - 1;
+
+            while (left < right)    // time log(n)
+            {
+                int currentSum = array[left] + array[right];
+                if (currentSum == remainingNumber)
+                {
+                    result.Add([array[i], array[left], array[right]]);
+                    left++;
+                    right--;
+                }
+                else if (currentSum < remainingNumber)
+                {
+                    left++;
+                }
+                else if (currentSum > remainingNumber)
+                {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
--- src/algoexpert/Program.cs
+++ src/algoexpert/Program.cs
@@ -6,15 +6,9 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        int[,] arr = {
-            { 0, 0, 0 },
-            { 0, 0, 0 }
-        };
-
-        // x => 1, y => 2
-        // x => 2, y => 1
-
-        TransposeMatrix.Run(arr);
+        int[] array = [12, 3, 1, 2, -6, 5, -8, 6];
+        int targetSum = 0;
+        List<int[]> result = ThreeSum.Run(array, targetSum);
 
         Console.ReadLine();
     }
