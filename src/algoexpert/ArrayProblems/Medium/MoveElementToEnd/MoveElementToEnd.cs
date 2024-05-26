0fce6df716f03d3b7618863d2c9aab62e8500a74
phyothiha-rezerv <phyothihacodes@gmail.com>
2024-05-26 16:46:38 +0700

    feat: add move element to end (answer)

--- src/algoexpert/ArrayProblems/Medium/MoveElementToEnd/MoveElementToEnd.cs
+++ src/algoexpert/ArrayProblems/Medium/MoveElementToEnd/MoveElementToEnd.cs
@@ -4,6 +4,32 @@ public class MoveElementToEnd
 {
     public static List<int> Run(List<int> array, int toMove)
     {
+        int left = 0;
+        int right = array.Count - 1;
+        while(left < right)
+        {
+            if(array[right] == toMove)
+            {
+                right--;
+                continue;
+            }
+            else
+            {
+                if (array[left] == toMove)
+                {
+                    int sum = array[left] + array[right];
+                    array[left] = sum - array[left];
+                    array[right] = sum - array[right];
+                    right--;
+                    left++;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+        }
+
         return array;
     }
 }
--- src/algoexpert/Program.cs
+++ src/algoexpert/Program.cs
@@ -6,10 +6,11 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        int[] arrayOne = [-1, 5, 10, 20, 28, 3];
-        int[] arrayTwo = [26, 134, 135, 15, 17];
-        int[] result = SmallestDifference.Run(arrayOne, arrayTwo);
+        List<int> arr = [2, 1, 2, 2, 3, 5, 4, 2, 2, 2];
+        int toMove = 2;
+        List<int> result = MoveElementToEnd.Run(arr, toMove);
 
+        result.ForEach(x => Debug.WriteLine(x));
         Console.ReadLine();
     }
 }
